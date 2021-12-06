43f55b31250bf1f3d6d299a449f2ad4f71dbd0a3
titipata <my.titipat@gmail.com>
2021-12-06 11:14:38 +0700

    Adding jpeg, png types to upload

--- st_dogbreed.py
+++ st_dogbreed.py
@@ -47,7 +47,7 @@ st.title("Dog Breed Classification")
 st.write("""
 🐶🐶🐶 Upload your dog image (or even yourself) to see what breed it is. 🐶🐶🐶
 """)
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
